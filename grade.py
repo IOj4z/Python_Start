9a97dbd320a2ce724cd168aa6c0804b07754a76d
Godfather-3113 <media.art.press2@gmail.com>
2022-11-16 23:14:02 +0500

    grade example

---
+++ grade.py
@@ -0,0 +1,10 @@
+score = int(input("Score: "))
+
+if 90 <= score <= 100:
+    print("Grade: A")
+elif 80 <= score < 90:
+    print("Grade: B")
+elif score >= 70 and score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
