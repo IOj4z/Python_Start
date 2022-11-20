cd5f00175db75d7cec8fa95271fcc0744c701c9e
Godfather-3113 <media.art.press2@gmail.com>
2022-11-20 21:58:42 +0500

    Libraries example

--- name.py
+++ name.py
@@ -1,3 +1,17 @@
 import sys
 
-print("hello, my name is ", sys.argv[1])
+# if len(sys.argv) < 2:
+#     # print("Too few arguments")
+#     sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     # print("Too many arguments")
+#     sys.exit("Too many arguments")
+# else:
+#     print("hello, my name is ", sys.argv[1])
+
+if len(sys.argv) < 2:
+    # print("Too few arguments")
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is ", arg)
