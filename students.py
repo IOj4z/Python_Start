7a4a57593bc676b8f0ff4ba9c01903ac2cd41431
Godfather-3113 <media.art.press2@gmail.com>
2022-11-21 13:46:11 +0500

    FILE I/O sort example

--- students.py
+++ students.py
@@ -1,4 +1,20 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+def get_name(student):
+    return student["name"]
+
+
+
+def get_house(student):
+    return student["house"]
+
+
+for student in sorted(students, key=get_house, reverse=True):
+    print(f"{student['name']} is in {student['house']}")
