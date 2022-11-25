7006eb167e6912b0b9776c86ef9c64e4cd957e9f
Godfather-3113 <media.art.press2@gmail.com>
2022-11-25 14:40:57 +0500

    Practice based on knowledge gained

---
+++ lesson/lesson_2.py
@@ -0,0 +1,37 @@
+# num = int(input('Enter number: '))
+# if num > 10:
+#     print("This is over 10")
+# else:
+#     print("this is not over 10")
+
+# if num > 10:
+#     print("This is over 10")
+# elif num == 10:
+#     print("This is equal to 10")
+# else:
+#     print("This is under 10")
+
+
+# if num >= 10:
+#     if num <= 20:
+#         print("This is between 10 and 20")
+#     else:
+#         print("This is over 20")
+# else:
+#     print("This is under 10")
+
+# text = str.lower(input('Enter upper text: '))
+# print(text)
+
+# num = int(input("Enter a number between 10 and 20: "))
+# if num >= 10 and num <= 20:
+# # if 10 <= num <= 20:
+#     print("Thank you")
+# else:
+#     print("Out of range")
+
+num = int(input("Enter an EVEN number between 1 and 5: "))
+if num == 2 or num == 4:
+    print("Thank you")
+else:
+    print("Incorrect")
