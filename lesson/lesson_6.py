2a6391bea91f9dadf0aeb2affa5f782968697743
Godfather-3113 <media.art.press2@gmail.com>
2022-11-26 14:05:04 +0500

    Practice based on knowledge WHILE

--- lesson/lesson_6.py
+++ lesson/lesson_6.py
@@ -1,5 +1,4 @@
 
-
 """
 Set total to 0. While total is 50 or less, prompt the user to enter a number.
 Add this number to total and print the message "The total is… [total]".
@@ -33,10 +32,30 @@ it goes on until until the user enters the answer is "y".
 After the cycle stops, print the amount.
 """
 
-a = int(input("Enter number A:"))
-b = int(input("Enter number B:"))
-total = a + b
-while input("Add one more? 'y|n'") == 'y':
-    c = int(input("Enter number C:"))
-    total += c
-print("Total ", total)
+# a = int(input("Enter number A:"))
+# b = int(input("Enter number B:"))
+# total = a + b
+# while input("Add one more? 'y|n'") == 'y':
+#     c = int(input("Enter number C:"))
+#     total += c
+# print("Total ", total)
+
+"""
+Prompt the user to enter the name of the person
+the user wants to invite to a party. 
+After that, print the message "[name] has been invited" and increment the counter by 1. 
+Ask if the user wants to invite someone more. 
+Keep prompting for names until the user will answer in the negative, 
+and print the number of invitees.
+"""
+count = 0
+
+while True:
+    person = input("Enter name to Invite person: ")
+    count = count + 1
+    print(f"{person} has been invited")
+    if input("Invite one more? 'y|n'") == 'y':
+        continue
+    else:
+        break
+print(f"{count} guest was invited")
