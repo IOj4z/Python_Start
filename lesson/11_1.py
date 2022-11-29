492961896158158d74778bb37edefbbaf45bab22
Godfather-3113 <media.art.press2@gmail.com>
2022-11-29 23:02:00 +0500

    Practice based on working with Two-Dimensional Lists and Dictionaries

--- lesson/11_1.py
+++ lesson/11_1.py
@@ -32,13 +32,37 @@ Ask if the user wants to change his.
 If the answer is yes, prompt to enter a new value and change the data. 
 Finally output again changed line.
 """
-userRow = int(input("Enter row index: "))
-print(nums[userRow])
-userColumn = int(input("Enter column index: "))
-print(nums[userRow][userColumn])
-if input('Do you want change? "yes|no": ') == 'yes':
-    userNewVal = int(input("Enter value: "))
-    nums[userRow][userColumn] = userNewVal
-    print(nums[userRow][userColumn])
+# userRow = int(input("Enter row index: "))
+# print(nums[userRow])
+# userColumn = int(input("Enter column index: "))
+# print(nums[userRow][userColumn])
+# if input('Do you want change? "yes|no": ') == 'yes':
+#     userNewVal = int(input("Enter value: "))
+#     nums[userRow][userColumn] = userNewVal
+#     print(nums[userRow][userColumn])
+#
+# print(nums[userRow])
 
-print(nums[userRow])
+"""
+Create the following dataset representing sales volumes by region, 
+in the form of a two-dimensional dictionary:
+"""
+data = {
+    'John': {'N': 3056, 'S': 8463, 'E': 8441, 'W': 2694},
+    'Tom': {'N': 4832, 'S': 6786, 'E': 4737, 'W': 3612},
+    'Anna': {'N': 5239, 'S': 4802, 'E': 5820, 'W': 1859},
+    'Fiona': {'N': 3904, 'S': 3645, 'E': 8821, 'W': 2451},
+}
+
+"""
+prompt the user for a name and region. 
+Output the relevant data. 
+Prompt the user for the name and region of the value they want to change, 
+and let them adjust the sales volume. 
+Display the sales volumes for all regions for
+the name chosen by the user.
+"""
+
+userName = input("Enter name: ")
+region = input("Enter region: ").upper()
+print(data[userName][region[0]])
