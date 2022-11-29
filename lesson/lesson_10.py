2fd6c38a80b58e947775bbd5025ac6219ac37687
Godfather-3113 <media.art.press2@gmail.com>
2022-11-29 13:30:03 +0500

    Practice based on working with Numeric arrays

---
+++ lesson/lesson_10.py
@@ -0,0 +1,37 @@
+from array import *
+
+nums = array('i', [45, 324, 654, 45, 264])
+# for x in nums:
+#     print(x)
+#
+# newValue = int(input("Enter number: "))
+# nums.append(newValue)
+# for x in nums:
+#     print(x)
+#
+# nums.reverse()
+# for x in nums:
+#     print(x)
+#
+# nums = sorted(nums)
+# for x in nums:
+#     print(x)
+#
+# nums.pop()
+# for x in nums:
+#     print(x)
+
+
+# newArray = array('i', [])
+# more = int(input('How many times: '))
+# for y in range(0, more):
+#     newValue = int(input('Enter num: '))
+#     newArray.append(newValue)
+# nums.extend(newArray)
+# newa = array('f', [45, 324, 654, 45, 264])
+# getRid = int(input("Enter item index: "))
+# print(nums)
+# del nums[getRid]
+# print(nums.count(45))
+# print(nums)
+
---
+++ lesson/lesson_10_1.py
@@ -0,0 +1,32 @@
+import random
+from array import *
+
+"""
+Prompt the user to enter whole numbers.
+If the user enters a number between 10 and 20, store it in an array;
+otherwise, print the message "Outside the range".
+After five numbers have been successfully added to the array,
+print the message "Thank you" and output an array, each
+whose element would be on a separate line.
+"""
+
+# nums = array('i', [])
+# while len(nums) != 5:
+#     nums.append(int(input("Enter num: ")))
+#
+# nums = sorted(nums)
+# nums.reverse()
+# print(nums)
+
+"""
+Create an array to store integers. 
+Generate five random numbers and store them in an array.
+Output the array (each element must be displayed on a separate line).
+"""
+integers = array('i', [])
+
+for i in range(5, 0, -1):
+    integers.append(random.randint(1, 10000))
+
+for x in integers:
+    print(x)
