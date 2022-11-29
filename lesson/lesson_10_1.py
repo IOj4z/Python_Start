f9e277e72f8cbcc8ef3e952844d9f9bd27f181f2
Godfather-3113 <media.art.press2@gmail.com>
2022-11-29 14:14:02 +0500

    Practice based on working with Numeric arrays

--- lesson/lesson_10_1.py
+++ lesson/lesson_10_1.py
@@ -58,8 +58,27 @@ Output the entire array.
 Suggest user to enter one of array numbers, 
 and then print a message in which indicated how many times a number occurs in this array.
 """
-nums = array('i', [2, 3, 6, 4, 4])
-for i in nums:
+# nums = array('i', [2, 3, 6, 4, 4])
+# for i in nums:
+#     print(i)
+# userNum = int(input('Enter one is number: '))
+# print(nums.count(userNum))
+
+"""
+Create two arrays: one will contain three
+the number entered by the user and the other is five random numbers. 
+Concatenate these two arrays into one big one. Sort and output it
+that each number must output in a separate line.
+"""
+
+userNums = array('i', [])
+integers = array('i', [])
+for i in range(5, 0, -1):
+    integers.append(random.randint(1, 10000))
+for i in range(3, 0, -1):
+    userNums.append(int(input('Enter number: ')))
+
+integers.extend(userNums)
+sorting = sorted(integers)
+for i in sorting:
     print(i)
-userNum = int(input('Enter one is number: '))
-print(nums.count(userNum))
