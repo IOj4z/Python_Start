d86c5eeeafc883e2d0d6b08da9fd7ac4556d9d48
Godfather-3113 <media.art.press2@gmail.com>
2022-12-11 00:00:52 +0500

    Practice based on working with Tkinter

--- lesson/lesson_15_tkinter.py
+++ lesson/lesson_15_tkinter.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 
 
@@ -22,30 +23,63 @@ When the user clicks the button, the window should display the message "Hello
 [name]" with background color and font color change text area.
 """
 
+#
+# def click():
+#     name = textbox1.get()
+#     message = str('Hello ' + name)
+#     textbox2['bg'] = 'yellow'
+#     textbox2['fg'] = 'blue'
+#     textbox2['text'] = message
+#
+#
+# window = Tk()
+# window.geometry("500x200")
+# window.title('Show name')
+# label1 = Label(text='Enter your name: ')
+# label1.place(x=30, y=20)
+#
+# textbox1 = Entry()
+# textbox1.place(x=150, y=20, width=200, height=25)
+# textbox1['justify'] = 'center'
+# textbox1.focus()
+#
+# button1 = Button(text='Press me', command=click)
+# button1.place(x=30, y=50, width=120, height=25)
+#
+# textbox2 = Message(text='')
+# textbox2.place(x=150, y=50, width=200, height=25)
+# textbox2['bg'] = 'white'
+# textbox2['fg'] = 'black'
+#
+# window.mainloop()
+
+
+"""
+Write a program throw simulation six-sided dice in a board game.
+When the user clicks the button, a random integer should be displayed on the screen
+number from 1 to 6
+(inclusive).
+"""
+
 
 def click():
-    name = textbox1.get()
-    message = str('Hello ' + name)
-    textbox2['bg'] = 'yellow'
-    textbox2['fg'] = 'blue'
-    textbox2['text'] = message
+    num = random.randint(1, 6)
+    textbox2['bg'] = 'white'
+    textbox2['fg'] = 'black'
+    textbox2['text'] = num
 
 
 window = Tk()
 window.geometry("500x200")
-window.title('Show name')
-label1 = Label(text='Enter your name: ')
+window.title('Show number')
+label1 = Label(text='Drop dice: ')
 label1.place(x=30, y=20)
 
-textbox1 = Entry()
-textbox1.place(x=150, y=20, width=200, height=25)
-textbox1['justify'] = 'center'
-textbox1.focus()
 
 button1 = Button(text='Press me', command=click)
 button1.place(x=30, y=50, width=120, height=25)
 
-textbox2 = Message(text='')
+textbox2 = Message()
 textbox2.place(x=150, y=50, width=200, height=25)
 textbox2['bg'] = 'white'
 textbox2['fg'] = 'black'
