ac37b64f6c1310d32e268a4c12c957008c94a152
Godfather-3113 <media.art.press2@gmail.com>
2022-12-10 16:47:53 +0500

    Practice based on working with Reading and writing CSV file

--- lesson/13_1.py
+++ lesson/13_1.py
@@ -47,15 +47,77 @@ If there are no books in the list this author, display the corresponding message
 Using the Books.csv file, prompt the user to enter a start and end year.
 Print all books published in the given time interval.
 """
-file = list(csv.reader(open('Books.csv')))
-bookYearStart = int(input('Enter start year: '))
-bookYearEnd = int(input('Enter end year: '))
+# file = list(csv.reader(open('Books.csv')))
+# bookYearStart = int(input('Enter start year: '))
+# bookYearEnd = int(input('Enter end year: '))
+# tmp = []
+# for row in file:
+#     tmp.append(row)
+# x = 0
+# for row in tmp:
+#     if int(tmp[x][2]) >= bookYearStart and int(tmp[x][2]) <= bookYearEnd:
+#         print(tmp[x])
+#     x = x + 1
+
+"""
+Using the Books.csv file, output the data with line numbers.
+"""
+# file = open('Books.csv', 'r')
+# x = 1
+# for row in file:
+#     string = str(x) + ') ' + row
+#     print(string)
+#     x = x + 1
+
+"""
+Import the data from the Books.csv file into the list. List it out, 
+prompt the user to choose which line they want to exclude, and delete it.
+Ask the user what data he wants to change and provide him
+appropriate opportunity. Write the data back to the .csv file, replacing the existing ones.
+"""
+file = list(csv.reader(open("Books.csv")))
 tmp = []
 for row in file:
     tmp.append(row)
+
+
 x = 0
-for row in tmp:
-    if int(tmp[x][2]) >= bookYearStart and int(tmp[x][2]) <= bookYearEnd:
-        print(tmp[x])
+for row in file:
+    string = x, tmp[x]
+    print(string)
     x = x + 1
 
+print(tmp)
+ids = int(input("Enter a row number to delete: "))
+del tmp[ids]
+x = 0
+for row in tmp:
+    display = x, tmp[x]
+    print(display)
+    x = x + 1
+
+alter = int(input("Enter a row number to alter: "))
+x = 0
+for row in tmp[alter]:
+    display = x, tmp[alter][x]
+    print(display)
+    x = x + 1
+
+part = int(input("Which part do you want to change? "))
+newdata = input("Enter new data: ")
+tmp[alter][part] = newdata
+print(tmp[alter])
+file = open("Books.csv", "w")
+
+x = 0
+for row in tmp:
+    newrecord = tmp[x][0] + ", " + tmp[x][1] + ", " + tmp[x][2] + "\n"
+    file.write(newrecord)
+    x = x+1
+file.close()
+"""
+Create a simple math game that asks the user for a name and then generates two random questions. 
+Save the name, questions entered, user responses, and final invoice in .csv file. 
+Each time it is run, the program should add information to the .csv file without
+overwriting existing data.
+"""
